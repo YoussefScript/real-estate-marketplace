ff693a3babdab680fd87b12a06a23b25c98127c6
youssef <youssef1362009kamel@gmail.com>
2026-09-11 18:03:41 +0300

    complete sign up page function & check if user exist in db

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,11 +3,34 @@ import bcryptjs from "bcryptjs";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+
   try {
+    // 1. Check if username already exists
+    const usernameExists = await User.findOne({ username });
+    if (usernameExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Username is already taken!",
+      });
+    }
+
+    // 2. Check if email already exists
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is already registered!",
+      });
+    }
+
+    // 3. Hash password and save if everything is fine
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+
     await newUser.save();
-    res.status(201).json({ message: "User created successfully!" });
+    res
+      .status(201)
+      .json({ success: true, message: "User created successfully!" });
   } catch (error) {
     next(error);
   }
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,15 @@ import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
